7d777d29cc3bfc32840519f06ea76bd7a9f5d169
Gustavo Zomer <gustavozomer@gmail.com>
2020-06-23 19:19:09 +0100

    Add method to auto toggle cardboard

--- js/vr.js
+++ js/vr.js
@@ -2,7 +2,9 @@ var VR = function() {
 
     const DEFAULT_PROJECTION = '360';
 
-    console.log(DEFAULT_PROJECTION);
+    this.log = new Log();
+
+    this.hasStartedCardboard = false
 
     this.hasDeviceOrientation = function() {
         return typeof DeviceMotionEvent != 'undefined' && typeof DeviceMotionEvent.requestPermission != "undefined"
@@ -55,25 +57,60 @@ var VR = function() {
         })
     }
 
-    this.showCardboardView = function() {
-        // Toggle cardboard
+    this.repeat = function (method, times) {
         var interval = setInterval(function(){
-            if ($("canvas").length > 0) {
+            if (times-- < 0 ) {
+                clearInterval(interval)
+                return
+            }
+            method()
+        }, 200)
+    }
+
+    this.showCardboardView = function() {
+        if (this.hasStartedCardboard) {
+            // Reset before switching view
+            $(".vjs-button-vr").trigger('click')
+        }
+
+        var interval = setInterval(function(){
+            if ($(".vjs-waiting").length == 0) {
                 setTimeout(function() {
-                    $(".vjs-button-vr").click()
+                    $(".vjs-button-vr").trigger('click')
                     this.resume()
-                }.bind(this),200)
+                    this.repeat(this.resume, 10)
+                    this.hasStartedCardboard = true;
+
+                    setTimeout(function() {
+                        $(".overlay").addClass('hidden')
+                    }, 500)
+                }.bind(this), 10)
                 clearInterval(interval)
             }
-        }.bind(this), 100)
+        }.bind(this), 10)
     }
 
     this.switchVideo = function(video) {
-        this.player.src({ src: video.src });
-        this.player.mediainfo.projection = video.projection || DEFAULT_PROJECTION;
-        this.player.play()
 
-        this.showCardboardView()
+        $(".overlay").removeClass('hidden')
+
+        this.player.src({ src: video.src });
+
+        this.player.mediainfo.projection = video.projection || DEFAULT_PROJECTION;
+
+        this.vr = this.player.vr({
+            motionControls:true,
+            projection: 'AUTO',
+            debug: true,
+            forceCardboard: true
+        });
+
+        this.player.play()
+        .then(function(){
+            this.showCardboardView()
+        }.bind(this), function(err){
+            this.log.warn('VR play error', err)
+        }.bind(this))
     }
 
     this.pause = function() {
@@ -88,14 +125,19 @@ var VR = function() {
             return
         }
         this.player.play()
+        .then(function(){
+
+        }.bind(this), function(err){
+            this.log.warn('VR play error', err)
+        }.bind(this))
     }
 
     this.start = function() {
-        this.player = window.player = videojs('videojs-vr-player');
-        player.mediainfo = player.mediainfo || {};
-        player.mediainfo.projection = DEFAULT_PROJECTION;
+        this.player = videojs('videojs-vr-player');
+        this.player.mediainfo = this.player.mediainfo || {};
+        this.player.mediainfo.projection = DEFAULT_PROJECTION;
 
-        this.vr = window.vr = player.vr({
+        this.vr = this.player.vr({
             motionControls:true,
             projection: 'AUTO',
             debug: true,
@@ -103,7 +145,12 @@ var VR = function() {
         });
 
         this.player.play()
+        .then(function(){
+            this.showCardboardView()
+        }.bind(this), function(err){
+            this.log.warn('VR play error', err)
+        }.bind(this))
 
-        this.showCardboardView()
+
     }
 }
